d9d08b42064743e3f84be72a4893b8f080da45d5
CodePlay2016 <13269308268@163.com>
2017-11-07 06:16:00 +0800

    serializable

--- src/main/java/com/thomas/buynsell/meta/BuyProduct.java
+++ src/main/java/com/thomas/buynsell/meta/BuyProduct.java
@@ -1,6 +1,8 @@
 package com.thomas.buynsell.meta;
 
-public class BuyProduct {
+import java.io.Serializable;
+
+public class BuyProduct implements Serializable {
     private int id;
     private int number;
 
